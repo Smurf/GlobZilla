c1d9f4815e5bb3e621cc02edba44c919ba58aef6
Smurf <sammuel.coles@gmail.com>
2010-05-01 23:57:05 -0700

    added alpha num

--- GlobMatcher.hs
+++ GlobMatcher.hs
@@ -10,12 +10,18 @@ matchGlob ss ('*':[])       = True
 matchGlob ss ('*':gs)       = True && matchGlob (dropWhile ((/=) $ head gs) ss) gs
 matchGlob (s:ss) ('?':gs)   = True && matchGlob ss gs
 
+matchGlob [] ('%':_)        = False
+matchGlob (s:ss) ('%':[])   = alphaNum s
+matchGlob (s:ss) ('%':gs)   = alphaNum s && matchGlob (dropWhile ((/=) $ head gs) ss) gs
+
+
 matchGlob ss ('[':gs)       = (charClass gs ss)
 
 matchGlob (s:ss) (g:gs)     = (s == g) && matchGlob ss gs
 
 matchGlob (s:ss) []         = False
 matchGlob [] []             = True
+matchGlob [] _              = True
 
 charClass :: String -> String -> Bool
 charClass  gs (s:ss)   = (s `elem` (cClass gs)) && (matchGlob ss gs'')
@@ -23,7 +29,13 @@ charClass  gs (s:ss)   = (s `elem` (cClass gs)) && (matchGlob ss gs'')
             cClass gs'          = takeWhile (/= ']') gs'
             gs''                = drop ((length $ cClass gs)+1) gs
 
+alphaNum s = or $ concat $ [checkAlpha, checkNum]
+    where   checkAlpha  = map (==s) $ concat [['a'..'z'],['A'..'Z']]
+            checkNum    = map (==s) ['0'..'9']
+
+
 test' = zipWith (matchGlob) searchStrs globs
 --Should return [True, False, True, False]
 globs       = ["f[il]le.*", "file?[aA][12]", "*.??[]aabc]c", "*.abc"]
 searchStrs  = ["file.xyz", "file.A.2", "asdf.ab]c", "asdf.dge"]
+
